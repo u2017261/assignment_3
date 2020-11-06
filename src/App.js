3b0beb979b691b17d3a6cf47218495a8f85d4dc3
u2017261 <73094750+u2017261@users.noreply.github.com>
2020-11-06 11:01:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> text changes by Hamza 261.
+          Edit <code>src/App.js</code> text changes by Hamza 261 in class.
         </p>
         <a
           className="App-link"
